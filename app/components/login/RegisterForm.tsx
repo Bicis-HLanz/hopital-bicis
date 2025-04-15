ae487c99a8fba1225635e6181185c756772bfcd0
mrdarip <61890952+mrdarip@users.noreply.github.com>
2025-04-15 16:36:19 +0000

    implemented RegisterForm

---
+++ app/components/login/RegisterForm.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import React, { useState } from "react";
+import styles from "./LoginForm.module.css";
+import { account, ID } from "../../appwrite.js";
+import Link from "next/link";
+import { AppwriteException } from "appwrite";
+
+interface User {
+  name: string;
+  email: string;
+}
+
+const LoginForm: React.FC = () => {
+  const [loggedInUser, setLoggedInUser] = useState<User | null>(null);
+  const [email, setEmail] = useState<string>("");
+  const [name, setName] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [repeatPassword, setRepeatPassword] = useState<string>("");
+
+  // Check if the user is already logged in
+  React.useEffect(() => {
+    const checkSession = async () => {
+      try {
+        const user = await account.get();
+        setLoggedInUser(user as User);
+      } catch (error) {
+        if (error instanceof AppwriteException && error.code === 401) {
+          // User is not logged in
+          setLoggedInUser(null);
+        } else {
+          console.error("Error checking session:", error);
+        }
+      }
+    };
+
+    checkSession();
+  }, []);
+
+  const register = async () => {
+    if (password !== repeatPassword) {
+      alert("Las contraseñas no coinciden");
+      return;
+    }
+    await account.create(ID.unique(), email, password, name);
+    login(email, password);
+  };
+
+  const login = async (email: string, password: string): Promise<void> => {
+    await account.createEmailPasswordSession(email, password);
+    setLoggedInUser(await account.get());
+  };
+
+  const logOut = async (): Promise<void> => {
+    await account.deleteSession("current");
+    setLoggedInUser(null);
+  };
+
+  if (loggedInUser) {
+    return (
+      <div>
+        <p>Sesión iniciada como {loggedInUser.name}</p>
+        <button type="button" onClick={logOut}>
+          Cerrar sesión
+        </button>
+      </div>
+    );
+  }
+
+  return (
+    <form className={styles["login-form"]}>
+      <h1>Iniciar Sesión</h1>
+      <input
+        type="email"
+        placeholder="Correo electrónico"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        className={styles["email-input"]}
+      />
+      <input
+        type="password"
+        placeholder="Contraseña"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        className={styles["email-input"]}
+      />
+      <input
+        type="password"
+        placeholder="Repetir contraseña"
+        value={repeatPassword}
+        onChange={(e) => setRepeatPassword(e.target.value)}
+        className={styles["email-input"]}
+      />
+      <input
+        type="text"
+        placeholder="Nombre"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        className={styles["email-input"]}
+      />
+      <button type="button" onClick={() => register()}>
+        Registrarse
+      </button>
+
+      <p className={styles["register-link"]}>
+        ¿No tienes cuenta aún?
+        <Link href="/registo">
+          Regístrate
+        </Link>
+      </p>
+    </form>
+  );
+};
+
+export default LoginForm;
