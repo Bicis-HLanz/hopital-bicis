a9c08ca724e447d9add0671d9acf39435273e84d
mrdarip <darioelas2005@gmail.com>
2025-04-18 12:32:38 +0200

    moved layout to (app)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import AppHeader from "./components/mainLayout/Header/AppHeader";
-import AppNav from "./components/mainLayout/Nav/AppNav";
-import AppFooter from "./components/mainLayout/Footer/AppFooter";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,10 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <AppHeader />
-        <main>{children}</main>
-        <AppFooter />
-        <AppNav />
+        {children}
       </body>
     </html>
   );
