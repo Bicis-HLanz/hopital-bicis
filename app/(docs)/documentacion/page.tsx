1e522b32579b14eab497ccb3aaa92c2c770d4e6b
mrdarip <darioelas2005@gmail.com>
2025-04-20 15:48:01 +0200

    add documentation page

---
+++ app/(docs)/documentacion/page.tsx
@@ -0,0 +1,8 @@
+export default function Page(){
+    return (
+        <>
+            <h1>Hospital de bicicletas</h1>
+            <p>Por Darío Peregrín Alconchel y Cristina Díaz Cabello</p>
+        </>
+    )
+}
