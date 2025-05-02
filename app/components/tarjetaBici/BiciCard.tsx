4c046897ead86a44fce673b913b8cabcac44fe19
mrdarip <darioelas2005@gmail.com>
2025-05-02 21:32:19 +0200

    fix: update BiciCard link to use document name instead of ID

--- app/components/tarjetaBici/BiciCard.tsx
+++ app/components/tarjetaBici/BiciCard.tsx
@@ -4,7 +4,7 @@ import { Models, } from "appwrite";
 
 export default function BiciCard({ doc }: { doc: Models.Document }) {
   return (
-    <Link href={`reservar/${doc.$id}`}>
+    <Link href={`reservar/${doc.name}`}>
       <Image src={doc["image-url"]} alt={doc.name} width={200} height={200} />
       <h2>{doc.name}</h2>
     </Link>
