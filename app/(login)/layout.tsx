671ecc8f8d476dba43902f3de0afcb1af670ede0
cristdc <cristinadiazcabello@gmail.com>
2025-05-08 20:43:44 +0200

    login and registres background added

--- app/(login)/layout.tsx
+++ app/(login)/layout.tsx
@@ -1,11 +1,17 @@
+import React from 'react';
+import styles from './layout.module.css';
+
 export default function RootLayout({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) {
-    return (
-        <main style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
-            {children}
-        </main>
-    );
-  }
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className={styles.container}>
+      <div className={styles.squareGrid} /> {/* ¡Cuadrados en movimiento! */}
+      <div className={styles.children}>
+        {children}
+      </div>
+    </div>
+  );
+}
