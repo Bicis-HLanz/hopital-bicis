680331dc134521a02ff5e073a8aee4dfcb09ff0f
mrdarip <darioelas2005@gmail.com>
2025-05-13 18:17:28 +0200

    feat: update bicycle image retrieval in BiciDetails component

--- app/(app)/reservar/[slug]/page.tsx
+++ app/(app)/reservar/[slug]/page.tsx
@@ -2,6 +2,7 @@ import { databases } from "@/appwriteServer";
 import Image from "next/image";
 import styles from "./page.module.css";
 import ReservarForm from "@/components/reservarForm/ReservarForm";
+import { getBycicleImage } from "@/appwrite";
 
 export const revalidate = 600;
 export const dynamicParams = true;
@@ -45,7 +46,7 @@ export default async function BiciDetails({ params }: Props) {
   return (
     <div className={styles["reservar-grid"]}>
       <div className={styles["details"]}>
-        <Image src={doc["image-url"]} alt={doc.name} width={400} height={400} />
+        <Image src={getBycicleImage(doc)} alt={doc.name} width={400} height={400} />
 
         <div className="details">
           <h1>{doc.name}</h1>
