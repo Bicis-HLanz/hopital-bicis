6c4529ea23876eaa1fd573268201fed99509e3a7
mrdarip <darioelas2005@gmail.com>
2025-05-20 11:47:10 +0200

    feat: implement bicycle management page with document listing functionality

--- app/(app)/gestionar/bicicletas/page.tsx
+++ app/(app)/gestionar/bicicletas/page.tsx
@@ -1,10 +1,24 @@
-export default function Page() {
+import { databases } from "@/appwriteServer"
+import { Bicycle } from "@/models/Bicycle"
+
+export default async function Page() {
+  const bicyclesList = await databases.listDocuments(
+    process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID as string,
+    process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_ID as string
+  ) as {
+    documents: Bicycle[]
+  };
+
   return (
-    <main>
-      <section>
-        <h1>Gestión de Bicicletas</h1>
-        <p>En esta sección podrás gestionar las bicicletas disponibles.</p>
-      </section>
-    </main>
+    <div>
+      <h1>Bicicletas</h1>
+      <div>
+        {bicyclesList.documents.map((bicycle) => (
+          <div key={bicycle.$id}>
+            <h2>{bicycle.name}</h2>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
--- app/(app)/gestionar/usuarios/page.tsx
+++ app/(app)/gestionar/usuarios/page.tsx
@@ -1,9 +1,5 @@
 import { users } from "@/appwriteServer";
 
-
-// Revalida cada 60 segundos (ISR)
-export const revalidate = 60
-
 export default async function Page() {
   const usersListResponse = await users.list()
   const usersList = usersListResponse.users
