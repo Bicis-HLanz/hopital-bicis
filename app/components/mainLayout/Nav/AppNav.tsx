06438c6caeb54243fc77d845fa5bca36681c151f
mrdarip <darioelas2005@gmail.com>
2025-05-20 11:55:25 +0200

    feat: restructure MisReservasPage and update navigation link

--- app/components/mainLayout/Nav/AppNav.tsx
+++ app/components/mainLayout/Nav/AppNav.tsx
@@ -16,7 +16,7 @@ const AppNav: React.FC = () => {
           </Link>
         </li>
         <li>
-          <Link href="/mi-reserva">
+          <Link href="/reservas">
             <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" className={styles["icono"]}>
               <rect x="3" y="4" width="18" height="18" rx="2" ry="2" />
               <path d="M16 2v4M8 2v4M3 10h18" />
