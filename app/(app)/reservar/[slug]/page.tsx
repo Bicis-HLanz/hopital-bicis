3d91c25838f393556d8e6a90914f5bffbb2c0efd
mrdarip <darioelas2005@gmail.com>
2025-05-21 19:01:46 +0200

    feat: refactor BiciDetails component to use state for bicycle data fetching

--- app/(app)/reservar/[slug]/page.tsx
+++ app/(app)/reservar/[slug]/page.tsx
@@ -1,64 +1,65 @@
+'use client'
+
 import { databases } from "@/appwriteServer";
 import Image from "next/image";
 import styles from "./page.module.css";
 import ReservarForm from "@/components/reservarForm/ReservarForm";
 import { getBycicleImage } from "@/appwrite";
 import { Bicycle } from "@/models/Bicycle";
+import React from "react";
 
-export const revalidate = 600;
-export const dynamicParams = false;
-
-export async function generateStaticParams() {
-  const databaseId = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID || "not_set";
-  const collectionId =
-    process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_ID || "not_set";
-
-  const docList: { documents: Bicycle[] } = await databases.listDocuments(
-    databaseId,
-    collectionId
-  );
-
-  return docList.documents.map((doc) => ({
-    slug: doc.$id,
-  }));
-}
-
-export default async function BiciDetails({
+export default function BiciDetails({
   params,
 }: {
   params: Promise<{ slug: string }>;
 }) {
-  const { slug } = await params;
+  const { slug } = React.use(params);
   const databaseId = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID;
   const collectionId = process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_ID;
+
+  const [bicycle, setBicycle] = React.useState<Bicycle | null>(null);
 
   if (!databaseId || !collectionId) {
     throw new Error("Database ID or Collection ID is not set");
   }
 
-  const doc : Bicycle = await databases.getDocument(
-    databaseId,
-    collectionId,
-    slug
-  );
+  React.useEffect(() => {
+    const fetchBicycle = async () => {
+      try {
+        const response = await databases.getDocument<Bicycle>(
+          databaseId,
+          collectionId,
+          slug
+        );
+        setBicycle(response);
+      } catch (error) {
+        console.error("Error fetching bicycle:", error);
+      }
+    }
+    fetchBicycle();
+  }, [slug, databaseId, collectionId]);
+
+  if (!bicycle) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className={styles["reservar-grid"]}>
       <div className={styles["details"]}>
         <Image
-          src={getBycicleImage(doc)}
-          alt={doc.name}
+          src={getBycicleImage(bicycle)}
+          alt={bicycle.name}
           width={150}
           height={150}
         />
 
         <div className={styles["details1"]}>
-          <h1>{doc.name}</h1>
-          {doc.description && <p>{doc.description}</p>}
+          <h1>{bicycle.name}</h1>
+          {bicycle.description && <p>{bicycle.description}</p>}
         </div>
       </div>
 
-      <ReservarForm bicycle={doc} />
+      <ReservarForm bicycle={bicycle} />
     </div>
   );
 }
